ec24b828337164fc24542b773eed35beecc686b7
Dhananjay Kumar <64013411+dhananjay229@users.noreply.github.com>
2026-05-21 23:18:40 +0530

    Initialize FastAPI app with task router

--- FastAPI_Task_Manager_API/app/main.py
+++ FastAPI_Task_Manager_API/app/main.py
@@ -1,1 +1,11 @@
+from fastapi import FastAPI
+from app.routers.task_router import router as task_router
+
+app = FastAPI(
+    title = "Task Manager API",
+    description = "Create a FastAPI Task Manager",
+    version = "1.0.0"
+)
+
+app.include_router(task_router)
 
